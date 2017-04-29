f2215b9caa66f3f3bcc4192b2b55f1a9249c639c
A-celer <764884989@qq.com>
2017-04-29 09:12:31 +0800

    添加验证码 URL

--- app/src/main/java/com/hotbitmapgg/bilibili/network/auxiliary/ApiConstants.java
+++ app/src/main/java/com/hotbitmapgg/bilibili/network/auxiliary/ApiConstants.java
@@ -2,34 +2,43 @@ package com.hotbitmapgg.bilibili.network.auxiliary;
 
 /**
  * Designed by hcc on 2016/11/20 21:32
-
+ *
  * <p>
  * API常量类
  */
 
 public class ApiConstants {
 
-  public static final String BILI_GO_BASE_URL = "http://bilibili-service.daoapp.io/";
+    //验证码 URL postfix
+    public static final String JPG = ".jpg";
 
-  public static final String RANK_BASE_URL = "http://www.bilibili.com/";
+    //bilibili 验证码 URL
+    public static final String BILI_VERIFY_URL = "https://passport.bilibili.com/captcha";
 
-  public static final String APP_BASE_URL = "http://app.bilibili.com/";
+    //验证码 URL , eg: http://ook0mwqpi.bkt.clouddn.com/image/jpg/1.jpg
+    public static final String VERIFY_BASE_URL = "http://ook0mwqpi.bkt.clouddn.com/image/jpg/";
 
-  public static final String LIVE_BASE_URL = "http://live.bilibili.com/";
+    public static final String BILI_GO_BASE_URL = "http://bilibili-service.daoapp.io/";
 
-  public static final String API_BASE_URL = "http://api.bilibili.cn/";
+    public static final String RANK_BASE_URL = "http://www.bilibili.com/";
 
-  public static final String BANGUMI_BASE_URL = "http://bangumi.bilibili.com/";
+    public static final String APP_BASE_URL = "http://app.bilibili.com/";
 
-  public static final String SEARCH_BASE_URL = "http://s.search.bilibili.com/";
+    public static final String LIVE_BASE_URL = "http://live.bilibili.com/";
 
-  public static final String ACCOUNT_BASE_URL = "https://account.bilibili.com/";
+    public static final String API_BASE_URL = "http://api.bilibili.cn/";
 
-  public static final String USER_BASE_URL = "http://space.bilibili.com/";
+    public static final String BANGUMI_BASE_URL = "http://bangumi.bilibili.com/";
 
-  public static final String VIP_BASE_URL = "http://vip.bilibili.com/";
+    public static final String SEARCH_BASE_URL = "http://s.search.bilibili.com/";
 
-  public static final String IM9_BASE_URL = "http://www.im9.com/";
+    public static final String ACCOUNT_BASE_URL = "https://account.bilibili.com/";
 
-  public static final String COMMON_UA_STR = "OhMyBiliBili Android Client/2.1 (100332338@qq.com)";
+    public static final String USER_BASE_URL = "http://space.bilibili.com/";
+
+    public static final String VIP_BASE_URL = "http://vip.bilibili.com/";
+
+    public static final String IM9_BASE_URL = "http://www.im9.com/";
+
+    public static final String COMMON_UA_STR = "OhMyBiliBili Android Client/2.1 (100332338@qq.com)";
 }
